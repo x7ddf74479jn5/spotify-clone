cf2fdd868e21dff1e2ce76c3b210dadbc743720e
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-12-19 15:54:07 +0900

    🔥 remove: scafolded files

--- src/components/ui/layout/Layout.tsx
+++ src/components/ui/layout/Layout.tsx
@@ -2,16 +2,13 @@ import type { CustomLayout } from "next";
 
 import { Footer } from "./Footer";
 import { Header } from "./Header";
-import { LayoutErrorBoundary } from "./LayoutErrorBoundary";
 
 export const Layout: CustomLayout = (page) => {
   return (
-    <>
+    <div className="flex min-h-screen bg-black max-w-[1500px]">
       <Header />
-      <main>
-        <LayoutErrorBoundary>{page}</LayoutErrorBoundary>
-      </main>
+      <main>{page}</main>
       <Footer />
-    </>
+    </div>
   );
 };
