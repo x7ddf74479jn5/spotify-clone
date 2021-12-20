8853d0ec026d63408cba6eb0c248784bae010280
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-12-21 01:12:45 +0900

    ✨ add: login page

--- src/components/page/Login/Login.tsx
+++ src/components/page/Login/Login.tsx
@@ -1,3 +1,30 @@
-export const Login = () => {
-  return <div></div>;
+import type { ClientSafeProvider } from "next-auth/react";
+import { signIn } from "next-auth/react";
+import type { VFC } from "react";
+import type { TNextAuthProviders } from "src/types/auth";
+
+type LoginProps = {
+  providers: TNextAuthProviders;
+};
+
+export const Login: VFC<LoginProps> = ({ providers }) => {
+  if (!providers) throw new Error();
+
+  const handleSignIn = (provider: ClientSafeProvider) => {
+    signIn(provider.id, { callbackUrl: "/" });
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center w-full min-h-screen bg-black">
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img src="/img/fPuEa9V.png" alt="" className="mb-5 w-52" />
+      {Object.values(providers).map((provider) => (
+        <div className="" key={provider.name}>
+          <button onClick={() => handleSignIn(provider)} className="p-5 text-white rounded-full bg-[#18D860]">
+            Login with {provider.name}
+          </button>
+        </div>
+      ))}
+    </div>
+  );
 };
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,10 +1,26 @@
-import type { CustomNextPage } from "next";
+import type { CustomNextPage, GetServerSideProps, InferGetServerSidePropsType } from "next";
+import { getProviders } from "next-auth/react";
 import { Login } from "src/components/page/Login";
+import type { TNextAuthProviders } from "src/types/auth";
 
-const LoginPage: CustomNextPage = () => {
-  return <Login />;
+type PageProps = InferGetServerSidePropsType<typeof getServerSideProps>;
+
+const LoginPage: CustomNextPage<PageProps> = ({ providers }) => {
+  return <Login providers={providers} />;
 };
 
-// LoginPage.getLayout = Layout;
-
 export default LoginPage;
+
+type ServerSideProps = {
+  providers: TNextAuthProviders;
+};
+
+export const getServerSideProps: GetServerSideProps<ServerSideProps> = async () => {
+  const providers = await getProviders();
+
+  return {
+    props: {
+      providers,
+    },
+  };
+};
