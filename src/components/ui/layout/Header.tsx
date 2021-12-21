ae3d8790da83f570022df8799f106ad337fed1ae
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-12-22 02:30:26 +0900

    ✨ add: Header

--- src/components/ui/layout/Header.tsx
+++ src/components/ui/layout/Header.tsx
@@ -1,10 +1,19 @@
-import type { VFC } from "react";
+import { ChevronDownIcon } from "@heroicons/react/outline";
+import { useSession } from "next-auth/react";
+import { memo } from "react";
 
-export const Header: VFC = () => {
+export const Header: React.VFC = memo(() => {
+  const { data: session } = useSession();
   return (
-    <header>
-      <h1>Title</h1>
-      <nav></nav>
+    <header className="absolute top-5 right-8">
+      <div className="flex items-center p-1 pr-2 space-x-3 bg-black rounded-full opacity-90 hover:opacity-80 cursor-pointer">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src={session?.user?.image ?? "/img/no-profile.png"} alt="" className="w-10 h-10 rounded-full" />
+        <h2>{session?.user.name}</h2>
+        <ChevronDownIcon className="w-5 h-5" />
+      </div>
     </header>
   );
-};
+});
+
+Header.displayName = "Header";
